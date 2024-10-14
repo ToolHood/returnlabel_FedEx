1a0c7387818257577016e51363c14695ad9e86dd
Christoph <61665083+YockerFX@users.noreply.github.com>
2024-10-14 10:16:01 +0200

    Update Colors

--- popup.js
+++ popup.js
@@ -1,16 +1,17 @@
 document.getElementById('autofillButton').addEventListener('click', function() {
     const selectedProduct = document.getElementById('productSelect').value;
-  
+    const selectedColor = document.getElementById('colorSelect').value;
+
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
       chrome.scripting.executeScript({
         target: {tabId: tabs[0].id},
         func: autofillForm,
-        args: [selectedProduct]  // Der ausgewählte Wert wird als Argument übergeben
+        args: [selectedProduct, selectedColor]
       });
     });
-  });
-  
-  function autofillForm(selectedProduct) {
+});
+
+function autofillForm(selectedProduct, selectedColor) {
     const data = {
       "toData.addressData.contactName": "David Maciol",
       "billingData.referenceData.purchaseOrderNumber": "Pro Gamersware",
@@ -20,7 +21,7 @@ document.getElementById('autofillButton').addEventListener('click', function() {
       "psdData.mpsRowDataList[0].weight": "1",
       "psdData.serviceType": "FedEx Ground",
       "psdData.mpsRowDataList[0].mpsDimensionSelect": "9010394", 
-      "billingData.referenceData.yourReference": selectedProduct  
+      "billingData.referenceData.yourReference": selectedProduct + " " + selectedColor  
     };
   
     for (let field in data) {
@@ -30,5 +31,4 @@ document.getElementById('autofillButton').addEventListener('click', function() {
         element.dispatchEvent(new Event('change'));
       }
     }
-  }
-  
+}
